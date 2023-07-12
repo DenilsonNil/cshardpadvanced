f99091d5e0dc1b19ab974df0972849c64492dad4
d.alves <d.alves@unlimint.com>
2023-07-11 22:25:11 -0300

    add exemplo de casting customizado

---
+++ C-Sharp-Advanced/CastingCustomizado/Casting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Casting
+{
+    public void TestCasting()
+    {
+        LetraAlfabeto la = new LetraAlfabeto('B');
+
+        //Usabilidade. Armarzenando em uma variável do tipo char uma variável do tipo Alfabeto
+        char c = (char)la;
+        Console.WriteLine(c);
+
+        LetraAlfabeto la2 = 'X';
+    }
+}
+
+class LetraAlfabeto
+{
+    char caractere;
+
+    public LetraAlfabeto(char caractere)
+    {
+        this.caractere = char.ToUpper(caractere);
+    }
+
+    public static explicit operator char(LetraAlfabeto la)
+    {
+        return la.caractere;
+    }
+
+    //Casting para converter um objeto de LetraAlfabeto para um char
+    public static implicit operator LetraAlfabeto(char c)
+    {
+        return new LetraAlfabeto(c);
+    }
+}
